9d3fafd82aeab30a615a5d65530cf090f3af3f82
jobayer <jobayerrahman0220@gmail.com>
2026-04-02 19:48:33 +0600

    chore(client): update Next.js config for Cloudinary images
    Adds Cloudinary domain to remotePatterns for external image optimization

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,10 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/uploads/**",
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
